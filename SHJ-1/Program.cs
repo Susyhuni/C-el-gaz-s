fcef60f943427f828243828931314e68ede11593
Sólyom Hunor János <SolyomHJ@KKSZKI.LOCAL>
2025-09-18 09:29:00 +0200

    Feladat6 ready

--- SHJ-1/Program.cs
+++ SHJ-1/Program.cs
@@ -32,13 +32,73 @@ namespace SHJ_1
             }
             else { Console.WriteLine("A szám nulla"); }
 
+
+            Console.WriteLine("Kérem adja meg a hónap számát (1-12):");
+            int honap = int.Parse(Console.ReadLine());
+            string evszak;
+            if (honap >= 1 && honap <= 12)
+            {
+                if (honap == 12 || honap == 1 || honap == 2)
+                    evszak = "Tél";
+                else if (honap >= 3 && honap <= 5)
+                    evszak = "Tavasz";
+                else if (honap >= 6 && honap <= 8)
+                    evszak = "Nyár";
+                else
+                    evszak = "Ősz";
+
+                Console.WriteLine($"A {honap}. hónap az {evszak} évszakhoz tartozik.");
+            }
+            else
+            {
+                Console.WriteLine("Hibás hónapszámot adott meg. Kérem, 1 és 12 között válasszon.");
+            }
+
             Console.WriteLine("Adja meg, hány éves");
             int szam4 = int.Parse(Console.ReadLine());
             if (szam4 > 18)
             { Console.WriteLine("Szavazhatsz!"); }
-            else { Console.WriteLine("nem szavazhatsz még"); }
+            else { Console.WriteLine("nem szavazhatsz még!"); }
 
+            Console.WriteLine("Adjon meg egy jegyet(1-5)");
+            int jegy = int.Parse(Console.ReadLine());
+            if (jegy >= 1 && jegy <= 5) 
+            {
+                if (jegy >= 1 && honap <= 2)
+                    Console.WriteLine("elégtelen");
+                else if (jegy == 3)
+                    Console.WriteLine("közepes");
+                else
+                    Console.WriteLine("Jeles");
+            }
+            else
+            {
+                Console.WriteLine("Nem létező jegyet  adott meg. Kérem, 1 és 5 között válasszon.");
+            }
 
+            int tárolt = 15;
+            int tipp;
+
+                Console.WriteLine("Próbáld meg kitalálni a számom");
+            while (true)
+            {
+                Console.Write("Add meg a tipped: ");
+                if (!int.TryParse(Console.ReadLine(), out tipp))
+                {
+                    Console.WriteLine("Számot adj meg");
+                    continue;
+                }
+
+                if (tipp < tárolt)
+                    Console.WriteLine("A titkos szám nagyobb.");
+                else if (tipp > tárolt)
+                    Console.WriteLine("A titkos szám kisebb.");
+                else
+                {
+                    Console.WriteLine("Gratulálok, eltaláltad!");
+                    break;
+                }
+            }
         }
     }
 }
