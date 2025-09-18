d7fcccc2c6388a9e8f58ec9072f4f64197fba099
Sólyom Hunor János <SolyomHJ@KKSZKI.LOCAL>
2025-09-18 08:45:07 +0200

    Feladat1-3 ready

--- SHJ-1/Program.cs
+++ SHJ-1/Program.cs
@@ -10,6 +10,30 @@ namespace SHJ_1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Adjon meg egy egész számot");
+            int szam = int.Parse(Console.ReadLine());
+            if (szam %2 ==0 )
+            {
+                Console.WriteLine("Ez a szám páros");
+            }
+            else
+            {
+                Console.WriteLine("Ez a szám páratlan");
+            }
+            Console.WriteLine("Adjon meg egy egész számot");
+            int szam2 = int.Parse(Console.ReadLine());
+            if (szam2 < 0)
+            {
+                Console.WriteLine("Ez a szám negatív");
+            }
+            else if (szam2 > 0)
+            {
+                Console.WriteLine("Ez a szám pozitív");
+            }
+            else { Console.WriteLine("A szám nulla"); }
+            
+            
+
         }
     }
 }
